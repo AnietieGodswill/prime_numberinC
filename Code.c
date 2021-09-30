188f6fdddc0e834a706f128748f7065984b1d291
Nishant Tiwari <dx4iot@gmail.com>
2021-10-01 01:13:40 +0530

    Update Code.c

--- Code.c
+++ Code.c
@@ -1,18 +1,37 @@
-#include<stdio.h>
-main() {
-    int n, i, count=0;
-    printf("Enter Number");
-    scanf("%d",&n);
-   
-    for ( i = 1; i<= n; i++) {
-        if (n % i == 1) {
-            count++;
+#include <iostream>
+#include <math.h>
+using namespace std;
+ 
+int main()
+{
+    int n, i, flag = 1;
+ 
+    // Ask user for input
+    cout <<"Enter a number: \n";
+ 
+    // Store input number in a variable
+    cin >> n ;
+ 
+    // Iterate from 2 to sqrt(n)
+    for (i = 2; i <= sqrt(n); i++) {
+ 
+        // If n is divisible by any number between
+        // 2 and n/2, it is not prime
+        if (n % i == 0) {
+            flag = 0;
+            break;
         }
     }
-    if( count == 2) {
-        printf("Prime Number");
+ 
+    if (n <= 1)
+        flag = 0;
+ 
+    if (flag == 1) {
+        cout << n <<" is a prime number";
     }
     else {
-        printf("Not A  Prime Number");
+        cout << n <<" is not a prime number";
     }
+ 
+    return 0;
 }
